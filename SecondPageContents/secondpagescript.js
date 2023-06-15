4b7c9cfad461d20fae563de074520111d61d4ab2
Grant <grantbooysen002@gmail.com>
2023-06-15 22:31:20 +0200

    Added in new academic, other, sport files that is linked to the second page. Modified the second page with additional buttons and links to the new pages.

--- SecondPageContents/secondpagescript.js
+++ SecondPageContents/secondpagescript.js
@@ -1,5 +1,25 @@
+// Get the elements
 const openButton = document.getElementById("openButton");
+const academicColumn = document.querySelector(".academic-column");
+const otherColumn = document.querySelector(".other-column");
+const sportColumn = document.querySelector(".sport-column");
 
+// Add event listener to the back button
 openButton.addEventListener("click", function() {
   window.location.href = "../FirstPageContents/frontPage.html";
+});
+
+// Assign URL to academic column
+academicColumn.addEventListener("click", function() {
+  window.location.href = "../AccessoryPage/academic.html";
+});
+
+// Assign URL to other column
+otherColumn.addEventListener("click", function() {
+  window.location.href = "../AccessoryPage/other.html";
+});
+
+// Assign URL to sport column
+sportColumn.addEventListener("click", function() {
+  window.location.href = "../AccessoryPage/sport.html";
 });
