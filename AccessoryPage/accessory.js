0349faa549d38055cc49c7fa8727567518eb3324
Grant <grantbooysen002@gmail.com>
2023-06-16 16:02:07 +0200

    Added in the following files:

---
+++ AccessoryPage/accessory.js
@@ -0,0 +1,8 @@
+// Get the elements
+const openButton = document.getElementById("openButton");
+
+// Add event listener to the back button
+openButton.addEventListener("click", function() {
+  window.location.href = "../SecondPageContents/secondPage.html";
+});
+
